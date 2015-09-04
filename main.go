377ae2500ec61ee754f9618d2a8d74cd0e1b1a4f
rolando <rsegura@gbm.net>
2015-09-04 14:04:03 -0600

    ready to run in Bluemix

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func appHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,8 +13,16 @@ func appHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	flagIP := flag.String("ip", "127.0.0.1", "ip address")
+	flagPort := flag.String("port", "6060", "tcp port")
+	flag.Parse()
+
+	if ip := os.Getenv("VCAP_APP_HOST"); ip != "" {
+		*flagIP = ip
+	}
+	if port := os.Getenv("VCAP_APP_PORT"); port != "" {
+		*flagPort = port
+	}
 	http.HandleFunc("/hwbm", appHandler)
-	ip := ""
-	port := "8080"
-	log.Fatal(http.ListenAndServe(ip+":"+port, nil))
+	log.Fatal(http.ListenAndServe(*flagIP+":"+*flagPort, nil))
 }
