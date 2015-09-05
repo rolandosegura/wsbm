ff3dc9c4f340795cfbb120ea4b7166f93d772806
rolando <rsegura@gbm.net>
2015-09-05 00:41:23 -0600

    watson translation services

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	mgo "gopkg.in/mgo.v2"
@@ -8,14 +9,45 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 )
 
-// HTTP Server para implementar el "distributed hash table"
+type watsonMTClient struct {
+	c           *http.Client
+	apiEndpoint string
+	apiUser     string
+	apiPwd      string
+}
+
+func (wmt *watsonMTClient) translate(source, target, text string) (string, error) {
+	var translation string
+	url := fmt.Sprintf("%s/v2/translate?source=%s&target=%s&text=%s", wmt.apiEndpoint, source, target, url.QueryEscape(text))
+	rq, err := http.NewRequest("POST", url, strings.NewReader(text))
+	if err != nil {
+		return translation, err
+	}
+	rq.SetBasicAuth(wmt.apiUser, wmt.apiPwd)
+	rq.Header.Set("content-type", "plain/text")
+	resp, err := wmt.c.Do(rq)
+	if err != nil {
+		return translation, err
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return translation, err
+	}
+	translation = string(b)
+	return translation, nil
+}
+
+// HTTP Server that implements the persistent hash table.
 type server struct {
 	dbSession *mgo.Session
 	dbName    string
+	wc        *watsonMTClient
 }
 
 func (s *server) put(v string) (bson.ObjectId, error) {
@@ -48,7 +80,7 @@ func (s *server) get(k string) (string, error) {
 	return data.Data, err
 }
 
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (s *server) storageHandler(w http.ResponseWriter, r *http.Request) {
 	urlcomp := strings.Split(r.URL.Path, "/")
 	switch r.Method {
 	case "GET":
@@ -86,10 +118,63 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (s *server) translateHandler(w http.ResponseWriter, r *http.Request) {
+	urlcomp := strings.Split(r.URL.Path, "/")
+	switch r.Method {
+	case "GET":
+		if len(urlcomp) < 4 {
+			http.Error(w, "no data found", http.StatusNotFound)
+			return
+		}
+		data, err := s.get(urlcomp[3])
+		switch err {
+		case nil:
+		case mgo.ErrNotFound:
+			http.Error(w, "no data found", http.StatusNotFound)
+			return
+		default:
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		translations := map[string]string{}
+		for _, lang := range []string{"es", "fr", "pt"} {
+			txl, err := s.wc.translate("en", lang, data)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			translations[lang] = txl
+		}
+		resp, err := json.MarshalIndent(&struct {
+			K            string            `json:"k"`
+			V            string            `json:"v"`
+			Translations map[string]string `json:"translations"`
+		}{
+			K:            urlcomp[3],
+			V:            data,
+			Translations: translations,
+		}, "", "	")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, "%s\n", resp)
+		return
+	default:
+		whdr := w.Header()
+		whdr.Add("Allow", "GET")
+		http.Error(w, "invalid http method", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
 func main() {
 	flagIP := flag.String("ip", "127.0.0.1", "ip address")
 	flagPort := flag.String("port", "6060", "tcp port")
 	flagMongo := flag.String("mongo", "localhost:27017/dkv", "mongodb URL")
+	flagWatsonURL := flag.String("wurl", "https://gateway.watsonplatform.net/language-translation/api", "Watson translation service API endpoint")
+	flagWatsonUser := flag.String("wuser", "de1e5046-c78a-4b4a-889e-9b016d0e2b14", "Watson translation service user name")
+	flagWatsonPwd := flag.String("wpwd", "oRj6v2QWqONC", "Watson translation service password")
 	flag.Parse()
 
 	if ip := os.Getenv("VCAP_APP_HOST"); ip != "" {
@@ -97,6 +182,15 @@ func main() {
 	}
 	if port := os.Getenv("VCAP_APP_PORT"); port != "" {
 		*flagPort = port
+	}
+	if wurl := os.Getenv("WMT_URL"); wurl != "" {
+		*flagMongo = wurl
+	}
+	if wusr := os.Getenv("WMT_USER"); wusr != "" {
+		*flagWatsonUser = wusr
+	}
+	if wpwd := os.Getenv("WMT_PWD"); wpwd != "" {
+		*flagWatsonPwd = wpwd
 	}
 	if m := os.Getenv("MONGOLAB_URI"); m != "" {
 		*flagMongo = m
@@ -109,7 +203,14 @@ func main() {
 	server := &server{
 		dbSession: session,
 		dbName:    session.DB("").Name,
+		wc: &watsonMTClient{
+			c:           http.DefaultClient,
+			apiEndpoint: *flagWatsonURL,
+			apiUser:     *flagWatsonUser,
+			apiPwd:      *flagWatsonPwd,
+		},
 	}
-	http.Handle("/dkv/", server)
+	http.HandleFunc("/dkv/", server.storageHandler)
+	http.HandleFunc("/dkv/translate/", server.translateHandler)
 	log.Fatal(http.ListenAndServe(*flagIP+":"+*flagPort, nil))
 }
